01aa520de7ba097dc8c945164aaaae30c6c0f0f8
Mohammed Nuved Khan <work.nuved32@gmail.com>
2025-11-12 19:19:28 +0530

    added day 1 python projects

---
+++ day1.py
@@ -0,0 +1,9 @@
+friends=["Nuved","ayisha","mazin"]
+print(friends)
+
+print(friends[1])
+
+friends.append=("khan")
+
+
+      
---
+++ python1.py
@@ -0,0 +1,4 @@
+num1= int(input("Enter the first number"))
+num2= int(input("Enter the second number"))
+sum= int(num1)+int(num2)
+print("the sum ",+sum)
